649cedd312e749627d517308f3ffd840fabc1784
ulziiiii <ulziisurendavaasuren@gmail.com>
2024-12-24 10:28:24 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import TodoPage from "./pages/TodoPage";
 import DndPage from "./pages/DndPage";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/VITE/">
       <Routes>
         <Route path="/" element={<TodoPage />} />
         <Route path="/dnd" element={<DndPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
